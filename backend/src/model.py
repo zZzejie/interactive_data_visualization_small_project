87f382897ab06be2cd02916621be705d1accdf21
zZzoo <zejieguo27@gmail.com>
2024-01-28 16:42:27 +0100

    add new files

---
+++ backend/src/model.py
@@ -0,0 +1,12 @@
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from typing import List
+class Company(BaseModel):
+        id: int
+        name: str
+        category: str
+        founding_year: int
+        employees: int
+        profit: List
+        def to_json(self):
+            return jsonable_encoder(self, exclude_none=True)
